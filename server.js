694d024553e429331ded6d704c7c047cc9a012d5
dipanshgarg21 <dipanshgarg21@gmail.com>
2023-03-02 10:09:25 +0530

    topic10 updated

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const base = `${__dirname}/public`;
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(`${base}/iot_applications.html`);
+});  
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
